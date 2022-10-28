b0bd40dcfb8d26c18968d021b333a9675d465866
otaciliob <otaciliobarbieri@gmail.com>
2022-10-28 12:13:44 -0300

    adição de metodos a classe emprestimo

--- integracao/src/main/java/model/beans/Emprestimo.java
+++ integracao/src/main/java/model/beans/Emprestimo.java
@@ -2,8 +2,44 @@ package model.beans;
 
 public class Emprestimo {
     
-    private String id;
-    private int rg;
-    private String titulo;
+    private String livro_id;
+    private int leitor_rg;
+    private String data_devolucao;
+
+    public Emprestimo() {
+    }
+
+    public Emprestimo(String livro_id, int leitor_rg, String data_devolucao) {
+        this.livro_id = livro_id;
+        this.leitor_rg = leitor_rg;
+        this.data_devolucao = data_devolucao;
+    }
+
+    public String getLivro_id() {
+        return livro_id;
+    }
+
+    public int getLeitor_rg() {
+        return leitor_rg;
+    }
+
+    public String getData_devolucao() {
+        return data_devolucao;
+    }
+
+    public void setLivro_id(String livro_id) {
+        this.livro_id = livro_id;
+    }
+
+    public void setLeitor_rg(int leitor_rg) {
+        this.leitor_rg = leitor_rg;
+    }
+
+    public void setData_devolucao(String data_devolucao) {
+        this.data_devolucao = data_devolucao;
+    }
+    
+    
+    
     
 }
---
+++ integracao/src/test/java/dao/LeitorTest.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package dao;
+
+import model.dao.LoginDao;
+import model.dao.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import org.junit.After;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TelaLoginTest {
+
+    static Connection conexao;
+    private final static String user = "login";
+    private final static String pass = "senha";
+    private static final String url ="jdbc:mysql://localhost:3306/teste";
+    LoginDao dao = new LoginDao(url);
+    
+    @Before
+    public void setup() throws SQLException {
+        conexao = ConnectionFactory.getConnection(url);
+        String sqllogininsert = "INSERT INTO login VALUES ('login','senha')";
+        PreparedStatement ps = conexao.prepareStatement(sqllogininsert);
+        ps.executeUpdate(sqllogininsert);
+    }
+
+    @Test
+    public void testLoginCorreto() {
+        assertEquals(true, dao.login(user, pass));
+    }
+
+    @Test
+    public void testLoginUsuarioESenhaIncorretos() {
+        assertEquals(false, dao.login("no", "no"));
+    }
+
+    @Test
+    public void testLoginUsuarioVazioSenhaCorreta() {
+        assertEquals(false, dao.login("", pass));
+    }
+
+    @Test
+    public void testLoginUsuarioCorretoSenhaVazia() {
+        assertEquals(false, dao.login(user, ""));
+    }
+
+    @Test
+    public void testLoginCorreto02() {
+        assertEquals(true, dao.login("login", "senha"));
+    }
+
+    @Test
+    public void testLoginUsuarioIncorretoESenhaCorreta() {
+        assertEquals(false, dao.login("user", "senha"));
+    }
+
+    @Test
+    public void testLoginCorretoESenhaIncorreta() {
+        assertEquals(false, dao.login("login", "123456"));
+    }
+
+    @Test
+    public void testLoginComNovoCadastroDeNovoUsuarioESenhaCorreto() throws SQLException {
+        String sqllogininsert = "INSERT INTO login VALUES ('teste','123')";
+        PreparedStatement ps = conexao.prepareStatement(sqllogininsert);
+        ps.executeUpdate(sqllogininsert);
+
+        assertEquals(true, dao.login("teste", "123"));
+
+        String sqllogindelete = "DELETE FROM login WHERE user = 'teste'";
+        ps = conexao.prepareStatement(sqllogindelete);
+        ps.executeUpdate(sqllogindelete);
+    }
+
+    @Test
+    public void testLoginComUsuarioCorretoESenhaVazia() {
+        assertEquals(false, dao.login("login", ""));
+    }
+
+    @Test
+    public void testLoginComNovoCadastroDeNovoUsuarioESenhaIncorreto() throws SQLException {
+        String sqllogininsert = "INSERT INTO login VALUES ('teste','123')";
+        PreparedStatement ps = conexao.prepareStatement(sqllogininsert);
+        ps.executeUpdate(sqllogininsert);
+
+        assertEquals(false, dao.login("false", "123"));
+
+        String sqllogindelete = "DELETE FROM login WHERE user = 'teste'";
+        ps = conexao.prepareStatement(sqllogindelete);
+        ps.executeUpdate(sqllogindelete);
+    }
+
+    @After
+    public void after() throws SQLException {
+        String sqllogindelete = "DELETE FROM login WHERE user = 'login'";
+        PreparedStatement ps = conexao.prepareStatement(sqllogindelete);
+        ps.executeUpdate(sqllogindelete);
+    }
+}
